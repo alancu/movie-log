ffca7e70f7206e5ac8bf666e9a58d2e22961ece1
Alejandro <alejandro.ca.348@gmail.com>
2025-05-07 14:57:48 +0200

    Connexió de prova TMDb

--- app/src/main/java/com/alejandro/movielog/MainActivity.kt
+++ app/src/main/java/com/alejandro/movielog/MainActivity.kt
@@ -6,7 +6,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +25,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // Prova de connexió anònima Firebase
+        /*
         FirebaseAuth.getInstance().signInAnonymously()
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
@@ -27,6 +35,30 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     Log.e("FIREBASE", "Error al connectar: ${task.exception}")
                 }
+
+         */
+
+        // Prova de connexió TMDb
+        // Crida segura en segon pla
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                try {
+                    val client = OkHttpClient()
+                    val request = Request.Builder()
+                        .url("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1")
+                        .get()
+                        .addHeader("accept", "application/json")
+                        .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YTVjYmZmMTQzZWRhOTBlNTk2NjIyODc4YWFhNjM1NCIsIm5iZiI6MTc0MzE1MjkzNy44MDQsInN1YiI6IjY3ZTY2NzI5NWYzZTBhYzE4ODAwNTBkNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BD-VZDhYzpiOnyOhnS6rKJJfkVlF9VieQVQ54f3dpZo")
+                        .build()
+
+                    val response = client.newCall(request).execute()
+                    val body = response.body()?.string()
+
+                    Log.d("TMDb", "Resposta: $body")
+                } catch (e: Exception) {
+                    Log.e("TMDb", "Error: ${e.message}")
+                }
             }
+        }
     }
 }
