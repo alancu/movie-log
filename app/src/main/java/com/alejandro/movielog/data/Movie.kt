7d3b137564ff490c2e3a63e96ec70057ad2835b4
Alejandro <alejandro.ca.348@gmail.com>
2025-05-08 13:43:45 +0200

    Movie.kt ara és 'parcelable'

--- app/src/main/java/com/alejandro/movielog/data/Movie.kt
+++ app/src/main/java/com/alejandro/movielog/data/Movie.kt
@@ -1,9 +1,13 @@
 package com.alejandro.movielog.data
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
 // classe que representa una pel·lícula rebuda de l'API
 // "data class" s'utilitza per a representar dades i crea algunes funcions per defecte
+// ho fem "parcelable" per poder enviar-ho entre activitats
+@Parcelize
 data class Movie (
     val id: Int,
     val title: String,
@@ -11,4 +15,4 @@ data class Movie (
     // @SerializedName perquè en el JSON aquest camp es diu 'poster_path', però ací l'anomenem posterPath
     @SerializedName("poster_path")
     val posterPath: String
-)
+) : Parcelable
