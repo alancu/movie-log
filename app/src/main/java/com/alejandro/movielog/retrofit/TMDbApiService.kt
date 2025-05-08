7b995cb889982ee722399c6393c2308ee3d0555c
Alejandro <alejandro.ca.348@gmail.com>
2025-05-08 13:14:38 +0200

    Afegits comentaris a TMDbApiService.kt

--- app/src/main/java/com/alejandro/movielog/retrofit/TMDbApiService.kt
+++ app/src/main/java/com/alejandro/movielog/retrofit/TMDbApiService.kt
@@ -4,7 +4,10 @@ import com.alejandro.movielog.data.MovieResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+// interfície que defineix les peticions a l'API de TMDb
 interface TMDbApiService {
+    // petició GET a l'endpoint 'movie/popular' de l'API per a obtindre pel·lícules populars
     @GET("movie/popular")
+    // 'suspend' indica que és una funció que es pot executar de manera asíncrona
     suspend fun getPopularMovies(@Query("api_key") apiKey: String): MovieResponse
 }
