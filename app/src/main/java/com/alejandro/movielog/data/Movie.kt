bf6d815f3fbf6e34ce931b356f20e33fb817a5b8
Alejandro <alejandro.ca.348@gmail.com>
2025-05-08 12:59:16 +0200

    Afegits comentaris a Movie.kt

--- app/src/main/java/com/alejandro/movielog/data/Movie.kt
+++ app/src/main/java/com/alejandro/movielog/data/Movie.kt
@@ -2,10 +2,13 @@ package com.alejandro.movielog.data
 
 import com.google.gson.annotations.SerializedName
 
+// 'data class' és una classe de Kotlin que per guardar dades
+// crea automàticament funcions útils
 data class Movie (
     val id: Int,
     val title: String,
     val overview: String,
+    // @SerializedName per què en el JSON, este camp es diu 'poster_path', però ací posterPath
     @SerializedName("poster_path")
     val posterPath: String?
 )
