21fb7972361bc96f0da095e8d74e954475b10d7c
Alejandro <alejandro.ca.348@gmail.com>
2025-05-08 13:47:22 +0200

    Lògica bàsica MovieDetailActivity.kt

--- app/src/main/java/com/alejandro/movielog/MovieDetailActivity.kt
+++ app/src/main/java/com/alejandro/movielog/MovieDetailActivity.kt
@@ -1,20 +1,32 @@
 package com.alejandro.movielog
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.alejandro.movielog.data.Movie
+import com.bumptech.glide.Glide
 
 class MovieDetailActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_movie_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Recupera l'objecte Movie passat a l'Intent
+        @Suppress("DEPRECATION")
+        val movie: Movie? = intent.getParcelableExtra("movie")
+
+        val titleTextView: TextView = findViewById(R.id.tv_movie_detail_title)
+        val descriptionTextView: TextView = findViewById(R.id.tv_movie_detail_description)
+        val posterImageView: ImageView = findViewById(R.id.iv_movie_detail_poster)
+
+        titleTextView.text = movie?.title
+        descriptionTextView.text = movie?.overview
+
+        // posem la imatge
+        Glide.with(this)
+            .load("https://image.tmdb.org/t/p/w500${movie?.posterPath}")
+            .into(posterImageView)
     }
 }
