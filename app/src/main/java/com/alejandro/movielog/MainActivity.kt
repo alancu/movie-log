09f621947f9ef5e60f5fcd827bf990ea514a54f5
Alejandro <alejandro.ca.348@gmail.com>
2025-05-08 14:21:18 +0200

    Eliminat codi comentat Afegit comentari explicatori

--- app/src/main/java/com/alejandro/movielog/MainActivity.kt
+++ app/src/main/java/com/alejandro/movielog/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.rvMovies)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+        // càrrega inicial de pel·lícules populars
         lifecycleScope.launch {
             try {
                 val response = RetrofitClient.apiService.getPopularMovies("4a5cbff143eda90e596622878aaa6354")
@@ -42,17 +43,5 @@ class MainActivity : AppCompatActivity() {
         // Crear l'adaptador
         adapter = MovieAdapter(movieList)
         recyclerView.adapter = adapter
-
-        /*
-        val movie1 = Movie(1, "Pelicula A", "Descripció A", "https://www.tallengestore.com/cdn/shop/products/Art_Poster_-_Sicario_-_Tallenge_Hollywood_Collection_large.jpg")
-        val movie2 = Movie(2, "Pelicula B", "Descripció B", "https://assets.mubicdn.net/images/notebook/post_images/38230/images-w1400.jpeg")
-
-        // Afegir les pel·lícules a la llista
-        movieList.add(movie1)
-        // Notificar al RecyclerView que les dades han canviat
-        adapter.notifyItemInserted(0)
-        movieList.add(movie2)
-        adapter.notifyItemInserted(1)
-        */
     }
 }
