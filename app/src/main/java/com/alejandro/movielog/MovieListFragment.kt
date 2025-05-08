3444f8979d29e85fc6febf5f209746d92d0ecc92
Alejandro <alejandro.ca.348@gmail.com>
2025-05-08 16:07:31 +0200

    Eliminada MovieRepository

--- app/src/main/java/com/alejandro/movielog/MovieListFragment.kt
+++ app/src/main/java/com/alejandro/movielog/MovieListFragment.kt
@@ -8,7 +8,6 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alejandro.movielog.data.Movie
-import com.alejandro.movielog.data.MovieRepository
 import com.alejandro.movielog.retrofit.RetrofitClient
 import com.alejandro.movielog.ui.MovieAdapter
 import kotlinx.coroutines.launch
@@ -18,7 +17,6 @@ class MovieListFragment : Fragment(R.layout.fragment_movie_list) {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: MovieAdapter
     private val movieList = mutableListOf<Movie>()
-    private val movieRepository = MovieRepository()
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: android.view.View, savedInstanceState: Bundle?) {
@@ -31,7 +29,7 @@ class MovieListFragment : Fragment(R.layout.fragment_movie_list) {
         // càrrega inicial de pel·lícules populars
         lifecycleScope.launch {
             try {
-                val response = movieRepository.getPopularMovies()
+                val response = RetrofitClient.apiService.getPopularMovies("4a5cbff143eda90e596622878aaa6354")
                 movieList.clear()
                 movieList.addAll(response.results)
                 adapter.notifyDataSetChanged()
