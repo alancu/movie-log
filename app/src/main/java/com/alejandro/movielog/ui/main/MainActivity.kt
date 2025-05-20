cd7d78aebe1c46a484fe3af53dfb654e1ccd91c6
Alejandro <alejandro.ca.348@gmail.com>
2025-05-20 14:38:14 +0200

    Adaptat MainActivity per a usar BaseActivity

--- app/src/main/java/com/alejandro/movielog/ui/main/MainActivity.kt
+++ app/src/main/java/com/alejandro/movielog/ui/main/MainActivity.kt
@@ -6,13 +6,9 @@ import com.alejandro.movielog.R
 import com.alejandro.movielog.ui.base.BaseActivity
 import com.alejandro.movielog.ui.fragments.MovieListFragment
 import com.alejandro.movielog.utils.Constants
-import com.alejandro.movielog.utils.auth.UserAuthHelper
 import com.alejandro.movielog.utils.navigateTo
 import dagger.hilt.android.AndroidEntryPoint
 import android.view.Menu
-import android.widget.ImageView
-import com.alejandro.movielog.ui.components.UserProfileDialog
-import com.alejandro.movielog.utils.loadCircularImage
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
@@ -34,7 +30,7 @@ class MainActivity : BaseActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
 
-        // Configura la SearchView
+        // Configura la SearchView només a MainActivity
         val searchItem = menu.findItem(R.id.action_search)
         val searchView = searchItem.actionView as androidx.appcompat.widget.SearchView
         searchView.queryHint = getString(R.string.search_movies)
@@ -50,21 +46,8 @@ class MainActivity : BaseActivity() {
             override fun onQueryTextChange(newText: String?) = false
         })
 
-        // Configura el botó de perfil d'usuari
-        val userItem = menu.findItem(R.id.action_user)
-        val actionView = userItem?.actionView
-        val userImage = actionView?.findViewById<ImageView>(R.id.image_user_menu)
-        val account = UserAuthHelper.getGoogleAccount(this)
-        account?.let {
-            userImage?.loadCircularImage(it.photoUrl.toString())
-        }
-        userImage?.setOnClickListener {
-            val dialog = UserProfileDialog(account) {
-                UserAuthHelper.logout(this)
-                finish()
-            }
-            dialog.show(supportFragmentManager, "user_profile")
-        }
+        // Crida la funció de baseActivity per a configurar el menú d'usuari
+        setupUserMenu(menu)
 
         return true
     }
