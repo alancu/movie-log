c4ccb5d52177838ddda1ae986d747ba15c57e234
Alejandro <alejandro.ca.348@gmail.com>
2025-05-20 14:58:34 +0200

    Adaptada UserProfileActivity per a usar BaseActivity Botons del perfil d'usuari fets redons

--- app/src/main/java/com/alejandro/movielog/ui/profile/UserProfileActivity.kt
+++ app/src/main/java/com/alejandro/movielog/ui/profile/UserProfileActivity.kt
@@ -1,21 +1,27 @@
 package com.alejandro.movielog.ui.profile
 
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.alejandro.movielog.R
+import com.alejandro.movielog.ui.base.BaseActivity
 import com.alejandro.movielog.ui.favorites.FavoriteMoviesActivity
 import com.alejandro.movielog.ui.history.WatchedMoviesActivity
 import com.alejandro.movielog.utils.auth.UserAuthHelper
 import com.alejandro.movielog.utils.loadCircularImage
 import com.alejandro.movielog.utils.navigateTo
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+        // Configura la toolbar amb títol i botó "enrere"
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setupToolbar(toolbar, getString(R.string.profile), showBack = true)
 
         val imageUserPhoto = findViewById<ImageView>(R.id.image_user_photo)
         val textUserName = findViewById<TextView>(R.id.text_user_name)
@@ -43,5 +49,16 @@ class UserProfileActivity : AppCompatActivity() {
         buttonWatched.setOnClickListener {
             navigateTo<WatchedMoviesActivity>()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_user, menu)
+        setupUserMenu(menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
     }
 }
