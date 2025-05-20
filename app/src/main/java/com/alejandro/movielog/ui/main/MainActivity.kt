6110ea90451e9624428860527eed36762595221f
Alejandro <alejandro.ca.348@gmail.com>
2025-05-20 14:18:37 +0200

    Modificada MainActivity per a usar BaseActivity Eliminada crida de proves a Firebase

--- app/src/main/java/com/alejandro/movielog/ui/main/MainActivity.kt
+++ app/src/main/java/com/alejandro/movielog/ui/main/MainActivity.kt
@@ -1,60 +1,45 @@
 package com.alejandro.movielog.ui.main
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
-import com.alejandro.movielog.ui.fragments.MovieListFragment
 import com.alejandro.movielog.R
-import com.alejandro.movielog.ui.components.UserProfileDialog
+import com.alejandro.movielog.ui.base.BaseActivity
+import com.alejandro.movielog.ui.fragments.MovieListFragment
 import com.alejandro.movielog.utils.Constants
 import com.alejandro.movielog.utils.auth.UserAuthHelper
-import com.alejandro.movielog.utils.loadCircularImage
 import com.alejandro.movielog.utils.navigateTo
 import dagger.hilt.android.AndroidEntryPoint
+import android.view.Menu
+import android.widget.ImageView
+import com.alejandro.movielog.ui.components.UserProfileDialog
+import com.alejandro.movielog.utils.loadCircularImage
 
-
-/**
- * Activitat principal de l'app. Mostra la llista de pel·lícules i gestiona la barra d'eines amb la cerca i el perfil d'usuari.
- */
-@Suppress("DEPRECATION")
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Configura la toolbar
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar?.title = getString(R.string.popular_movies)
+        setupToolbar(toolbar, getString(R.string.popular_movies), showBack = false)
 
-        // Mostra el fragment amb la llista de pel·lícules
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_movie_list, MovieListFragment())
                 .commit()
         }
-
-        //TODO proves
-        if (UserAuthHelper.isUserLoggedIn) {
-            com.alejandro.movielog.utils.firebase.FirestoreHelper.testWriteData()
-        }
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
+
+        // Configura la SearchView
         val searchItem = menu.findItem(R.id.action_search)
-        val searchView = searchItem.actionView as SearchView
+        val searchView = searchItem.actionView as androidx.appcompat.widget.SearchView
         searchView.queryHint = getString(R.string.search_movies)
 
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 if (!query.isNullOrBlank()) {
                     navigateTo<SearchActivity>(Constants.Extras.EXTRA_QUERY to query)
@@ -62,22 +47,17 @@ class MainActivity : AppCompatActivity() {
                 }
                 return true
             }
-
             override fun onQueryTextChange(newText: String?) = false
         })
 
-        // Carrega la imatge de perfil
+        // Configura el botó de perfil d'usuari
         val userItem = menu.findItem(R.id.action_user)
         val actionView = userItem?.actionView
         val userImage = actionView?.findViewById<ImageView>(R.id.image_user_menu)
-
         val account = UserAuthHelper.getGoogleAccount(this)
         account?.let {
             userImage?.loadCircularImage(it.photoUrl.toString())
-
         }
-
-        // Mostra el diàleg de perfil
         userImage?.setOnClickListener {
             val dialog = UserProfileDialog(account) {
                 UserAuthHelper.logout(this)
@@ -85,26 +65,7 @@ class MainActivity : AppCompatActivity() {
             }
             dialog.show(supportFragmentManager, "user_profile")
         }
+
         return true
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_user -> {
-                if (UserAuthHelper.isUserLoggedIn) {
-                    val account = UserAuthHelper.getGoogleAccount(this)
-                    val dialog = UserProfileDialog(account) {
-                        UserAuthHelper.logout(this)
-                        finish()
-                    }
-                    dialog.show(supportFragmentManager, "UserProfileDialog")
-                } else {
-                    Toast.makeText(this, "No s'ha iniciat sessió", Toast.LENGTH_SHORT).show()
-                }
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
 }
