62018654bc2cc484781c1e1fb4356b077b9cf598
Alejandro <alejandro.ca.348@gmail.com>
2025-05-20 19:22:51 +0200

    Modificats comentaris a MovieLogApp

--- app/src/main/java/com/alejandro/movielog/MovieLogApp.kt
+++ app/src/main/java/com/alejandro/movielog/MovieLogApp.kt
@@ -4,9 +4,8 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 /**
- * Classe d’aplicació principal
- * Esta classe s’executa abans que qualsevol altra part de l’app
- * i s’utilitza per a inicialitzar biblioteques globals com Hilt.
+ * Esta classe s’executa abans que qualsevol altra part de l’app.
+ * S’utilitza per a inicialitzar Hilt (injecció de dependències).
  */
-@HiltAndroidApp // indica que Hilt ha de gestionar la injecció de dependències en tota l'app
+@HiltAndroidApp // Indica a Hilt que use aquesta classe per inicialitzar la injecció de dependències
 class MovieLogApp : Application()
