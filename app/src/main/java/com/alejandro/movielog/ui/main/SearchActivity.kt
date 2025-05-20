5f4971056ad9239bb4b4e32f68dccd97d6815cdf
Alejandro <alejandro.ca.348@gmail.com>
2025-05-20 14:42:52 +0200

    Adaptada SearchActivity per a usar BaseActivity

--- app/src/main/java/com/alejandro/movielog/ui/main/SearchActivity.kt
+++ app/src/main/java/com/alejandro/movielog/ui/main/SearchActivity.kt
@@ -1,19 +1,20 @@
 package com.alejandro.movielog.ui.main
 
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.alejandro.movielog.R
 import com.alejandro.movielog.databinding.ActivitySearchBinding
+import com.alejandro.movielog.ui.base.BaseActivity
 import com.alejandro.movielog.ui.components.MovieAdapter
 import com.alejandro.movielog.utils.Constants
 import com.alejandro.movielog.viewmodel.MovieViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SearchActivity : AppCompatActivity() {
-
+class SearchActivity : BaseActivity() {
     private lateinit var binding: ActivitySearchBinding
     private val viewModel: MovieViewModel by viewModels()
     private lateinit var adapter: MovieAdapter
@@ -23,11 +24,10 @@ class SearchActivity : AppCompatActivity() {
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // configurem la toolbar
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // Configura la toolbar amb títol i botó "enrere"
+        setupToolbar(binding.toolbar, getString(R.string.search_results), showBack = true)
 
-        // obtenim la consulta des de l'Intent
+        // Obté la consulta de cerca
         val query = intent.getStringExtra(Constants.Extras.EXTRA_QUERY)
         if (!query.isNullOrEmpty()) {
             supportActionBar?.title = "\"$query\""
@@ -39,7 +39,7 @@ class SearchActivity : AppCompatActivity() {
         binding.rvSearchResults.layoutManager = LinearLayoutManager(this)
         binding.rvSearchResults.adapter = adapter
 
-        // Observem els resultats de la cerca
+        // Observa resultats de la cerca
         viewModel.movies.observe(this) { movies ->
             adapter.updateMovies(movies)
         }
@@ -49,10 +49,12 @@ class SearchActivity : AppCompatActivity() {
                 Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
             }
         }
+    }
 
-        if (!query.isNullOrEmpty()) {
-            viewModel.searchMovies(query)
-        }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_user, menu)
+        setupUserMenu(menu)
+        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
